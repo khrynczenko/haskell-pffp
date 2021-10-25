b3b7f65698ac819bb100b21cd3c81ab41a6053b6
Krzysztof Hrynczenko <jeniopy@gmail.com>
2021-10-25 22:53:14 +0200

    add chapter 11 cardinality exercises

---
+++ chapter11/exercises-cardinality.hs
@@ -0,0 +1,20 @@
+-- 1.
+data PugType = PugData
+-- Cardinality = 1 (only one possible value)
+
+
+-- 2.
+data Ariline = PapuAir
+             | CatapultR'Us
+             | TakeYourChancesUnited
+-- This is a sum type (discujnction) so we add carinalities od data constructors
+-- 1 + 1 + 1 = 3
+
+
+-- 3. The cardinality of Int16 is 2^16 = 65536
+
+-- 4. Int has a cardinality od Int32 and Integer does not have cardinality
+-- defined as it is infinite.
+
+-- 5. It is because the type cardinality is 2^8=256 as it occupies 8 bits (one
+-- byte.
